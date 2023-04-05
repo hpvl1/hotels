98c744891b6ed4240abb79798cf17d97d9c762cc
Pavel <pvlwrk@gmail.com>
2023-04-05 21:55:32 +0300

    update SCSS

--- src/components/UI/HotelElement/HotelElement.vue
+++ src/components/UI/HotelElement/HotelElement.vue
@@ -5,7 +5,7 @@ import { loadHotels } from '/src/api/index.js';
 
 import HotelName from './HotelName.vue';
 import HotelPrice from './HotelPrice.vue';
-import HotelCountry from './HotelCountry.vue'
+import HotelCountry from './HotelCountry.vue';
 import HotelType from './HotelType.vue';
 import HotelReviews from './HotelReviews.vue';
 import HotelDescription from './HotelDescription.vue';
@@ -15,28 +15,27 @@ const props = defineProps({
   hotels: {
     type: [Array, Object],
     default: [],
-  }
+  },
 });
 
 const hotels = reactive(props.hotels);
-
 </script>
 
 <template>
   <div class="hotel">
     <!-- <div v-for="(hotel, index) in hotels" :key="index" class="hotel"> -->
-        <HotelName class="hotel__name">Name: {{ hotels.name }}</HotelName>
-        <HotelPrice class="hotel__price">Price: {{ hotels.min_price }}</HotelPrice>
-        <HotelCountry class="hotel__country">Country: {{ hotels.country  }}</HotelCountry>
-        <HotelType class="hotel__type">Type: {{ hotels.type}}</HotelType>
-        <HotelReviews class="hotel__reviews">Reviews: {{ hotels.reviews_amount }}</HotelReviews>
-        <HotelDescription class="hotel__description">Desc: {{ hotels.description }}</HotelDescription>
-        <HotelStars class="hotel__stars" v-model="hotels.stars"/>
+    <HotelName class="hotel__name">Name: {{ hotels.name }}</HotelName>
+    <HotelPrice class="hotel__price">Price: {{ hotels.min_price }}</HotelPrice>
+    <HotelCountry class="hotel__country">Country: {{ hotels.country }}</HotelCountry>
+    <HotelType class="hotel__type">Type: {{ hotels.type }}</HotelType>
+    <HotelReviews class="hotel__reviews">Reviews: {{ hotels.reviews_amount }}</HotelReviews>
+    <HotelDescription class="hotel__description">Desc: {{ hotels.description }}</HotelDescription>
+    <HotelStars class="hotel__stars" v-model="hotels.stars" />
     <!-- </div> -->
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .hotel {
   width: 835px;
   height: 155px;
@@ -44,9 +43,9 @@ const hotels = reactive(props.hotels);
   padding: 15px 15px;
   border: 1px solid #eaeaea;
   border-radius: 15px;
-}
 
-.hotel__stars {
-  margin-top: -120px;
+  &__stars {
+    margin-top: -120px;
+  }
 }
 </style>
